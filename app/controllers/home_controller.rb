2c92db89897e3a489daa00153c10068929cf14f3
Alex Kovshovik <legend@hey.com>
2023-12-27 13:23:29 -0700

    Move expenses functionality to the authenticated expenses controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  before_action :redirect_authenticated_user
+
   def index
+  end
+
+  private
+
+  def redirect_authenticated_user
+    redirect_to expenses_path if user_signed_in?
   end
 end
