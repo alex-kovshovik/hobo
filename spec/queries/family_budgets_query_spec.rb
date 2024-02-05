fc82c9df03fbde07a2e4aafaa1ceb80bb9ceb6c4
Alex Kovshovik <legend@hey.com>
2024-02-04 19:43:23 -0700

    #1: Improve dark vs light theme styling and weekend highlights

--- spec/queries/family_budgets_query_spec.rb
+++ spec/queries/family_budgets_query_spec.rb
@@ -5,13 +5,13 @@ require "rails_helper"
 describe "FamilyBudgetsQuery" do
   let(:user) { create(:user) }
   let(:family) { create(:family, owner: user) }
-  let(:budget) { create(:budget, family: family) }
+  let(:budget) { create(:budget, family:) }
 
   before do
-    user.update(family: family)
+    user.update(family:)
 
-    create(:expense, budget: budget, date: Date.current)
-    create(:expense, budget: budget, date: Date.current - 1.month)
+    create(:expense, budget:, date: Date.current)
+    create(:expense, budget:, date: Date.current - 1.month)
   end
 
   it "returns family for a given date" do
