0e4014fd1afca636fa45d6f52f913bcc25bdb160
Alex Kovshovik <legend@hey.com>
2024-03-24 21:46:48 -0600

    Add Devise views and fix double-tap issue on iPhone

--- config/initializers/honeybadger.rb
+++ config/initializers/honeybadger.rb
@@ -1,6 +1,6 @@
 # i.e. config/initializers/honeybadger.rb
 Honeybadger.configure do |config|
-  config.api_key = ENV.fetch("HONEYBADGER_API_KEY")
+  config.api_key = ENV.fetch("HONEYBADGER_API_KEY", "")
   config.env = Rails.env
   config.root = Rails.root.to_s
 
