77cd32a25a49a066aa905c0155d0a6cdb593f2cc
Alex Kovshovik <legend@hey.com>
2025-01-20 12:19:50 -0700

    Update seeds and create script to import data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,17 @@
-password_digest = BCrypt::Password.create("12341234")
-
 user = User.create!(
   email_address: "test@test.com",
-  password_digest: password_digest,
-
+  password: "12341234",
   first_name: "Tester",
   last_name: "Test"
 )
 
 family = Family.create!(
-  name: "Test Family Group",
+  name: "Kovshoviks Family",
 )
 
 user.update!(family: family)
 
-Budget.create!(family: family, name: "Food", icon: "fa-solid fa-cart-shopping", amount: 1200)
+Budget.create!(family: family, name: "Food", icon: "fa-solid fa-cart-shopping", amount: 1800)
 Budget.create!(family: family, name: "Gas", icon: "fa-solid fa-gas-pump", amount: 150)
-Budget.create!(family: family, name: "Entertainment", icon: "fa-solid fa-volleyball", amount: 300)
+Budget.create!(family: family, name: "Lunch", icon: "fa-solid fa-utensils", amount: 300)
+Budget.create!(family: family, name: "Badass", icon: "fa-solid fa-capsules", amount: 300)
---
+++ scripts/import_expenses_from_csv.rb
@@ -0,0 +1,26 @@
+# The CSV file was produced by the following SQL:
+#
+# select
+#   e.id,
+#   b.name as budget,
+#   e.amount,
+#   e.date,
+#   u.first_name
+# from expenses e
+# join budgets b on b.id = e.budget_id
+# join users u on u.id = e.created_by
+
+require "csv"
+
+CSV.foreach("../expenses.csv", headers: true) do |row|
+  budget = Budget.find_by(name: row["budget"])
+  next if budget.nil?
+
+  user = User.find_by!(first_name: row["first_name"])
+
+  budget.expenses.create!(
+    amount: row["amount"],
+    date: row["date"],
+    created_by: user.id
+  )
+end
