a48c4fc1d938153f89736745aef868be7073f109
Alex Kovshovik <legend@hey.com>
2025-01-21 09:07:22 -0700

    Add service works to enable PWA on mobile

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+    // console.log('Service Worker registered with scope:', registration.scope);
+  }).catch(function (error) {
+    // console.log('Service Worker registration failed:', error);
+  });
+}
