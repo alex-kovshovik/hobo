a1131dcccd3be12681f0b024f9989a44666b94f6
Alex Kovshovik <legend@hey.com>
2025-01-21 09:15:54 -0700

    Experiment with PWA: change the start URL

--- app/views/pwa/service-worker.js
+++ app/views/pwa/service-worker.js
@@ -4,31 +4,31 @@ self.addEventListener('install', (event) => {
   // console.log('Service Worker installing.');
 });
 
-self.addEventListener('fetch', (event) => {
-  // console.log('Fetching:', event.request.url);
-});
+// self.addEventListener('fetch', (event) => {
+//   // console.log('Fetching:', event.request.url);
+// });
 
-// self.addEventListener("push", async (event) => {
-//   const { title, options } = await event.data.json()
-//   event.waitUntil(self.registration.showNotification(title, options))
-// })
+self.addEventListener("push", async (event) => {
+  const { title, options } = await event.data.json()
+  event.waitUntil(self.registration.showNotification(title, options))
+})
 
-// self.addEventListener("notificationclick", function(event) {
-//   event.notification.close()
-//   event.waitUntil(
-//     clients.matchAll({ type: "window" }).then((clientList) => {
-//       for (let i = 0; i < clientList.length; i++) {
-//         let client = clientList[i]
-//         let clientPath = (new URL(client.url)).pathname
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close()
+  event.waitUntil(
+    clients.matchAll({ type: "window" }).then((clientList) => {
+      for (let i = 0; i < clientList.length; i++) {
+        let client = clientList[i]
+        let clientPath = (new URL(client.url)).pathname
 
-//         if (clientPath == event.notification.data.path && "focus" in client) {
-//           return client.focus()
-//         }
-//       }
+        if (clientPath == event.notification.data.path && "focus" in client) {
+          return client.focus()
+        }
+      }
 
-//       if (clients.openWindow) {
-//         return clients.openWindow(event.notification.data.path)
-//       }
-//     })
-//   )
-// })
+      if (clients.openWindow) {
+        return clients.openWindow(event.notification.data.path)
+      }
+    })
+  )
+})
