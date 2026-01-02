f65676159d75384fd3bde91a54c02ce851264427
Alex Kovshovik <legend@hey.com>
2026-01-01 21:29:52 -0700

    Fix broadcast stream name to match subscription

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -11,6 +11,6 @@ class Budget < ApplicationRecord
   def broadcast_update(date)
     return if Rails.env.test?
 
-    broadcast_render_to family, date: date, partial: "budgets/update", locals: { budget: self, date: date }
+    broadcast_render_to family, date: date.beginning_of_month, partial: "budgets/update", locals: { budget: self, date: date }
   end
 end
